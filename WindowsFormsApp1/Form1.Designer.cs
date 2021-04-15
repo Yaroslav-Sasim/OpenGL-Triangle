dfa6665e60421d89ef810c945cdba679ea3d837d
Yaroslav-Sasim <62548404+Yaroslav-Sasim@users.noreply.github.com>
2021-04-15 19:37:45 +0300

    OpenGL Triangle
    Example of using and connecting OpenGL in C#

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openGLControl1 = new SharpGL.OpenGLControl();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl1
+            // 
+            this.openGLControl1.DrawFPS = false;
+            this.openGLControl1.Location = new System.Drawing.Point(56, 32);
+            this.openGLControl1.Name = "openGLControl1";
+            this.openGLControl1.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl1.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl1.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl1.Size = new System.Drawing.Size(647, 397);
+            this.openGLControl1.TabIndex = 0;
+            this.openGLControl1.Load += new System.EventHandler(this.openGLControl1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.openGLControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControl1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpGL;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+          
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    
+
+        private void openGLControl1_Load(object sender, EventArgs e)
+        {
+
+            
+
+
+            // Создаем экземпляр
+            OpenGL gl = this.openGLControl1.OpenGL;
+
+            // Очистка экрана и буфера глубин
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            // Сбрасываем модельно-видовую матрицу
+            gl.LoadIdentity();
+
+            // Двигаем перо вглубь экрана
+            gl.Translate(0.0f, 0.0f, -5.0f);
+
+            gl.Begin(OpenGL.GL_TRIANGLES);
+
+            // Указываем цвет вершин
+            gl.Color(3f, 1f, 1f);
+
+            gl.Vertex(-1f, -1f);
+            gl.Vertex(0f, 1f);
+            gl.Vertex(1f, -1f);
+
+            // Завершаем работу
+            gl.End();
+        }
+    }
+}
